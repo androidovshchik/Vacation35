8fd6eac90f14307f02f5dfdc305f60bc07daeeda
vlad <vladkalyuzhnyu@gmail.com>
2022-10-31 14:08:10 +0600

    login

---
+++ client/src/main/java/rf/vacation35/screen/LoginScreen.kt
@@ -0,0 +1,35 @@
+package rf.vacation35.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import rf.vacation35.databinding.ActivityLoginBinding
+import rf.vacation35.databinding.FragmentLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
