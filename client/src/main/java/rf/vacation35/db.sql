96f96d7d9487655cc5c438bc233094de95c198ee
vlad <vladkalyuzhnyu@gmail.com>
2022-11-04 10:39:11 +0600

    sql

---
+++ client/src/main/java/rf/vacation35/db.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users(
+    u_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    u_name VARCHAR(60) NOT NULL,
+    u_login VARCHAR(30) NOT NULL UNIQUE,
+    u_password VARCHAR(30) NOT NULL,
+    u_access_booking BOOLEAN NOT NULL DEFAULT FALSE,
+    u_access_price BOOLEAN NOT NULL DEFAULT FALSE,
+    u_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (u_id)
+) ENGINE=InnoDB CHARACTER SET=UTF8;
+
+INSERT INTO
+    users(u_name, u_login, u_password, u_access_booking, u_access_price, u_is_admin)
+VALUES
+    ('Админ', 'admin', 'отпуск35рф', TRUE, TRUE, TRUE);
+
+CREATE TABLE bases(
+    ba_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ba_name VARCHAR(60) NOT NULL,
+    PRIMARY KEY (ba_id)
+) ENGINE=InnoDB CHARACTER SET=UTF8;
+
+CREATE TABLE buildings(
+    bu_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    bu_name VARCHAR(60) NOT NULL,
+    bu_color VARCHAR(10) NOT NULL,
+    bu_base VARCHAR(10) NOT NULL,
+    PRIMARY KEY (bu_id),
+    FOREIGN KEY (bu_base) REFERENCES bases(ba_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+) ENGINE=InnoDB CHARACTER SET=UTF8;
+
+CREATE TABLE bookings(
+    bo_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    bo_entry_time DATETIME NOT NULL,
+    bo_exit_time DATETIME NOT NULL,
+    bo_phone VARCHAR(40) NOT NULL,
+    bo_alert BOOLEAN NOT NULL DEFAULT TRUE,
+    bo_bid BOOLEAN NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (bo_id)
+) ENGINE=InnoDB CHARACTER SET=UTF8;
