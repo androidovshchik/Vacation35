262839f60e0e548e02ff510530b295a0ba2cd14a
vlad <vladkalyuzhnyu@gmail.com>
2022-11-04 15:46:33 +0600

    login

--- client/src/main/java/rf/vacation35/screen/LoginScreen.kt
+++ client/src/main/java/rf/vacation35/screen/LoginScreen.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import rf.vacation35.databinding.ActivityLoginBinding
 import rf.vacation35.databinding.FragmentLoginBinding
 import rf.vacation35.local.Preferences
@@ -43,14 +48,20 @@ class LoginFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.iToolbar.appBar.isVisible = true
+        binding.iToolbar.toolbar.title = "Авторизация"
         binding.btnLogin.setOnClickListener {
             val login = binding.etLogin.text.toString()
             val password = binding.etPassword.text.toString()
-            val user = dbApi.findUser(login, password)
-            if (user != null) {
-                getView()?.snack("Найден")
-            } else {
-                getView()?.snack("Пользователь не найден")
+            viewLifecycleOwner.lifecycleScope.launch {
+                val user = withContext(Dispatchers.IO) {
+                    dbApi.findUser(login, password)
+                }
+                if (user != null) {
+                    getView()?.snack("Найден")
+                } else {
+                    getView()?.snack("Пользователь не найден")
+                }
             }
         }
     }
