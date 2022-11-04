1b3dc5ceaac7c567d6e2ad6a6729b211803d7720
vlad <vladkalyuzhnyu@gmail.com>
2022-11-04 11:30:34 +0600

    sql

--- client/src/main/java/rf/vacation35/db.sql
+++ client/src/main/java/rf/vacation35/db.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS buildings;
+DROP TABLE IF EXISTS bases;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users(
     u_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
     u_name VARCHAR(60) NOT NULL,
@@ -10,33 +15,38 @@ CREATE TABLE users(
 ) ENGINE=InnoDB CHARACTER SET=UTF8;
 
 INSERT INTO
-    users(u_name, u_login, u_password, u_access_booking, u_access_price, u_is_admin)
+    users(u_name, u_login, u_password, u_access_booking, u_access_price, u_admin)
 VALUES
     ('Админ', 'admin', 'отпуск35рф', TRUE, TRUE, TRUE);
 
 CREATE TABLE bases(
-    ba_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ba_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
     ba_name VARCHAR(60) NOT NULL,
     PRIMARY KEY (ba_id)
 ) ENGINE=InnoDB CHARACTER SET=UTF8;
 
 CREATE TABLE buildings(
-    bu_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    bu_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    bu_base MEDIUMINT UNSIGNED NOT NULL,
     bu_name VARCHAR(60) NOT NULL,
     bu_color VARCHAR(10) NOT NULL,
-    bu_base VARCHAR(10) NOT NULL,
     PRIMARY KEY (bu_id),
     FOREIGN KEY (bu_base) REFERENCES bases(ba_id)
         ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        ON UPDATE CASCADE
 ) ENGINE=InnoDB CHARACTER SET=UTF8;
 
 CREATE TABLE bookings(
     bo_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    bo_building MEDIUMINT UNSIGNED,
     bo_entry_time DATETIME NOT NULL,
     bo_exit_time DATETIME NOT NULL,
+    bo_client_name VARCHAR(200) NOT NULL,
     bo_phone VARCHAR(40) NOT NULL,
     bo_alert BOOLEAN NOT NULL DEFAULT TRUE,
     bo_bid BOOLEAN NOT NULL DEFAULT TRUE,
-    PRIMARY KEY (bo_id)
+    PRIMARY KEY (bo_id),
+    FOREIGN KEY (bo_building) REFERENCES buildings(bu_id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 ) ENGINE=InnoDB CHARACTER SET=UTF8;
