e5802fdb59541605ebb004ce708f3cfbbed6efd5
Vlad <vladkalyuzhnyu@gmail.com>
2022-11-14 10:57:47 +0600

    screen

--- client/src/main/java/rf/vacation35/screen/MainActivity.kt
+++ client/src/main/java/rf/vacation35/screen/MainActivity.kt
@@ -1,6 +1,7 @@
 package rf.vacation35.screen
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -8,18 +9,26 @@ import androidx.core.view.GravityCompat
 import androidx.lifecycle.lifecycleScope
 import com.android.calendar.month.MonthByWeekFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import rf.vacation35.R
 import rf.vacation35.databinding.ActivityMainBinding
 import rf.vacation35.databinding.DrawerHeaderBinding
 import rf.vacation35.extension.addFragment
 import rf.vacation35.extension.areYouSure
 import rf.vacation35.local.Preferences
+import rf.vacation35.remote.DbApi
 import splitties.activities.start
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AbstractActivity() {
+
+    @Inject
+    lateinit var api: DbApi
 
     @Inject
     lateinit var preferences: Preferences
@@ -28,7 +37,6 @@ class MainActivity : AbstractActivity() {
 
     private lateinit var header: DrawerHeaderBinding
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (preferences.rawUser == null) {
@@ -76,16 +84,43 @@ class MainActivity : AbstractActivity() {
 
         fragmentManager.addFragment(R.id.fl_container, MonthByWeekFragment(System.currentTimeMillis(), false))
 
+        updateHeader()
         lifecycleScope.launch {
             preferences.asFlow()
                 .collect {
                     if (it == "user") {
-                        val rawUser = preferences.rawUser!!
-                        header.login.text = "@${rawUser.login}"
-                        header.name.text = rawUser.name
+                        updateHeader()
                     }
                 }
         }
+        lifecycleScope.launch {
+            while (!isFinishing) {
+                try {
+                    val rawUser = preferences.rawUser!!
+                    val user = withContext(Dispatchers.IO) {
+                        api.findUser(rawUser.login, rawUser.password)
+                    }
+                    if (user != null) {
+                        preferences.rawUser = user.raw
+                    } else if (!isFinishing) {
+                        start<LoginActivity> {
+                            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        }
+                    }
+                } catch (e: Throwable) {
+                    Timber.e(e)
+                } finally {
+                    delay(60_000L)
+                }
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateHeader() {
+        val rawUser = preferences.rawUser!!
+        header.login.text = "@${rawUser.login}"
+        header.name.text = rawUser.name
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
