3da08fd8336a24559d7f533c774051b50e4ec985
Vlad <vladkalyuzhnyu@gmail.com>
2022-11-20 11:25:23 +0600

    changes

--- client/src/main/java/rf/vacation35/screen/BuildingScreen.kt
+++ client/src/main/java/rf/vacation35/screen/BuildingScreen.kt
@@ -1,6 +1,8 @@
 package rf.vacation35.screen
 
 import android.annotation.SuppressLint
+import android.app.TimePickerDialog
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.github.dhaval2404.colorpicker.ColorPickerDialog
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -19,9 +22,10 @@ import rf.vacation35.databinding.FragmentListBinding
 import rf.vacation35.databinding.ItemBuildingBinding
 import rf.vacation35.extension.*
 import rf.vacation35.remote.DbApi
-import rf.vacation35.remote.dao.BuildingDao
+import rf.vacation35.remote.dao.Building
 import splitties.fragments.start
 import splitties.snackbar.snack
+import java.time.LocalTime
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -46,7 +50,7 @@ class BuildingListFragment : Fragment() {
     private var listJob: Job? = null
 
     @SuppressLint("SetTextI18n")
-    private val adapter = abstractAdapter<ItemBuildingBinding, BuildingDao> {
+    private val adapter = abstractAdapter<ItemBuildingBinding, Building.Raw> {
         onCreateViewHolder { parent ->
             with(ItemBuildingBinding.inflate(layoutInflater, parent, false)) {
                 AbstractAdapter.ViewHolder(this).apply {
@@ -54,7 +58,7 @@ class BuildingListFragment : Fragment() {
                         try {
                             val item = items[bindingAdapterPosition]
                             start<BuildingActivity> {
-                                putExtra("id", item.id.value)
+                                putExtra("id", item.id)
                             }
                         } catch (ignored: Throwable) {
                         }
@@ -63,7 +67,9 @@ class BuildingListFragment : Fragment() {
             }
         }
         onBindViewHolder { item ->
+            color.setBackgroundColor(Color.parseColor(item.color))
             name.text = item.name
+            base.text = item.base.name
         }
     }
 
@@ -73,6 +79,7 @@ class BuildingListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val id = activity?.intent?.getIntExtra("id", 0) ?: 0
         with(binding.toolbar) {
             onBackPressed {
                 activity?.finish()
@@ -81,17 +88,20 @@ class BuildingListFragment : Fragment() {
         }
         binding.rvList.adapter = adapter
         binding.fabAdd.setOnClickListener {
-            start<BuildingActivity>()
+            start<BuildingActivity> {
+                putExtra("id", id)
+            }
         }
     }
 
     override fun onStart() {
         super.onStart()
+        val id = activity?.intent?.getIntExtra("id", 0) ?: 0
         listJob?.cancel()
         listJob = viewLifecycleOwner.lifecycleScope.launch {
             childFragmentManager.with(R.id.fl_fullscreen, progress, {
                 val items = withContext(Dispatchers.IO) {
-                    api.list(BuildingDao)
+                    api.listBuildings(id)
                 }
                 adapter.items.clear()
                 adapter.items.addAll(items)
@@ -127,7 +137,15 @@ class BuildingFragment : Fragment() {
 
     private lateinit var binding: FragmentBuildingBinding
 
-    private var building: BuildingDao? = null
+    private var building: Building? = null
+
+    private var color: String? = null
+
+    private var entry: LocalTime? = null
+
+    private var exit: LocalTime? = null
+
+    private var findJob: Job? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentBuildingBinding.inflate(inflater, container, false)
@@ -141,6 +159,32 @@ class BuildingFragment : Fragment() {
                 activity?.finish()
             }
             title = if (id == 0) "Новая постройка" else "Постройка"
+        }
+        binding.vColor.setOnClickListener {
+            ColorPickerDialog.Builder(requireActivity())
+                .setColorListener { value, hex ->
+                    color = hex
+                    binding.vColor.setBackgroundColor(value)
+                }
+                .customShow()
+        }
+        binding.tilEntry.isEndIconCheckable = id <= 0
+        binding.tilEntry.setEndIconOnClickListener {
+            TimePickerDialog(requireActivity(), { _, hourOfDay, minute ->
+                entry = LocalTime.of(hourOfDay, minute)
+            }, entry?.hour ?: 0, entry?.minute ?: 0, true).show()
+        }
+        binding.tilExit.isEndIconCheckable = id <= 0
+        binding.tilExit.setEndIconOnClickListener {
+            TimePickerDialog(requireActivity(), { _, hourOfDay, minute ->
+                exit = LocalTime.of(hourOfDay, minute)
+            }, exit?.hour ?: 0, exit?.minute ?: 0, true).show()
+        }
+        binding.btnBids.setOnClickListener {
+
+        }
+        binding.btnBookings.setOnClickListener {
+
         }
         binding.btnDelete.setOnClickListener {
             context?.areYouSure {
@@ -156,15 +200,18 @@ class BuildingFragment : Fragment() {
                 }
             }
         }
+        binding.btnSave.isEnabled = id <= 0
         binding.btnSave.setOnClickListener {
             try {
                 val name = binding.etName.text.toString().trim().ifEmpty { throw Throwable("Не задано имя") }
+                val color = color ?: throw Throwable("Не задан цвет")
                 viewLifecycleOwner.lifecycleScope.launch {
                     childFragmentManager.with(R.id.fl_fullscreen, progress, {
                         withContext(Dispatchers.IO) {
                             if (building == null) {
-                                building = api.create(BuildingDao) {
+                                building = api.create(Building) {
                                     it.name = name
+                                    it.color = name
                                 }
                             } else {
                                 api.update(building!!) {
@@ -182,26 +229,32 @@ class BuildingFragment : Fragment() {
                 getView()?.snack(e)
             }
         }
-        if (id > 0) {
-            viewLifecycleOwner.lifecycleScope.launch {
+        context?.updateRecentColors()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val id = activity?.intent?.getIntExtra("id", 0) ?: 0
+        if (id > 0 || building != null) {
+            findJob?.cancel()
+            findJob = viewLifecycleOwner.lifecycleScope.launch {
                 childFragmentManager.with(R.id.fl_fullscreen, progress, {
                     building = withContext(Dispatchers.IO) {
-                        api.find(BuildingDao, id)
+                        api.find(Building, id)
                     }
                     building?.let {
                         binding.etName.setText(it.name)
+                        binding.vColor.setBackgroundColor(Color.parseColor(it.color))
                         binding.btnDelete.isEnabled = true
                         binding.btnSave.isEnabled = true
                     }
                     if (building == null) {
-                        getView()?.snack("Постройка не найдена")
+                        view?.snack("Постройка не найдена")
                     }
                 }, {
-                    getView()?.snack(it)
+                    view?.snack(it)
                 })
             }
-        } else {
-            binding.btnSave.isEnabled = true
         }
     }
 
