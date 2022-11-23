4d05b51039d35f7b375cd0d870c87d0ab8829bbc
Vlad <vladkalyuzhnyu@gmail.com>
2022-11-23 14:12:56 +0600

    changes

--- client/src/main/java/rf/vacation35/screen/FilterFragment.kt
+++ client/src/main/java/rf/vacation35/screen/FilterFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -18,12 +20,15 @@ import rf.vacation35.remote.dao.Base
 import rf.vacation35.remote.dao.Building
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.math.max
 
 @AndroidEntryPoint
 class FilterFragment : Fragment() {
 
     @Inject
     lateinit var api: DbApi
+
+    val buildings = MutableSharedFlow<List<Building.Raw>>(0, 1, BufferOverflow.DROP_OLDEST)
 
     private lateinit var binding: FragmentFilterBinding
 
@@ -37,12 +42,12 @@ class FilterFragment : Fragment() {
             buildings.tryEmit(emptyList())
             when (position) {
                 0 -> binding.esBuilding.updateList(emptyList())
-                1 -> binding.esBuilding.updateList(buildings.map { it.name })
+                1 -> binding.esBuilding.updateList(mBuildings.value.map { it.name })
                 else -> {
-                    val base = bases.getOrNull(max(0, position - 2))
+                    val base = mBases.value.getOrNull(max(0, position - 2))
                     if (base != null) {
-                        //val buildings = buildings.filter { it.base == base.id.value }
-                        //binding.esBuilding.updateList(buildings.map { it.name })
+                        val buildings = mBuildings.value.filter { it.base.id == base.id.value }
+                        binding.esBuilding.updateList(buildings.map { it.name })
                     }
                 }
             }
@@ -50,20 +55,18 @@ class FilterFragment : Fragment() {
         }
         binding.esBuilding.setOnItemClickListener { _, _, position, _ ->
             when (position) {
-                0 -> {}
-                1 -> {}
+                0 -> buildings.tryEmit(emptyList())
+                1 -> buildings.tryEmit(mBuildings.value)
                 else -> {
-
+                    val building = mBuildings.value.getOrNull(max(0, position - 2))
+                    if (building != null) {
+                        buildings.tryEmit(listOf(building))
+                    }
                 }
             }
         }
         viewLifecycleOwner.lifecycleScope.launch {
             mBases.collect { items ->
-                binding.esBase.updateList(items.map { it.name })
-            }
-        }
-        viewLifecycleOwner.lifecycleScope.launch {
-            mBuildings.collect { items ->
                 binding.esBase.updateList(items.map { it.name })
             }
         }
@@ -75,14 +78,14 @@ class FilterFragment : Fragment() {
         withContext(Dispatchers.IO) {
             while (true) {
                 try {
+                    mBuildings.value = withContext(Dispatchers.IO) {
+                        DbApi.getInstance()
+                            .listBuildings()
+                            .sortedBy { it.name }
+                    }
                     mBases.value = withContext(Dispatchers.IO) {
                         DbApi.getInstance()
                             .list(Base)
-                            .sortedBy { it.name }
-                    }
-                    mBuildings.value = withContext(Dispatchers.IO) {
-                        DbApi.getInstance()
-                            .list(Building)
                             .sortedBy { it.name }
                     }
                     break
@@ -99,6 +102,6 @@ class FilterFragment : Fragment() {
 
         private val mBases = MutableStateFlow(listOf<Base>())
 
-        private val mBuildings = MutableStateFlow(listOf<Building>())
+        private val mBuildings = MutableStateFlow(listOf<Building.Raw>())
     }
 }
