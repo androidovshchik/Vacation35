808b93c0d8cd3d7f81233bc5c8d98dd58372ad25
Vlad <vladkalyuzhnyu@gmail.com>
2022-11-23 14:32:30 +0600

    changes

--- client/src/main/java/rf/vacation35/screen/CalendarScreen.kt
+++ client/src/main/java/rf/vacation35/screen/CalendarScreen.kt
@@ -7,12 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import rf.vacation35.R
 import rf.vacation35.databinding.FragmentCalendarBinding
 import rf.vacation35.databinding.ItemMonthBinding
 import rf.vacation35.extension.*
 import rf.vacation35.remote.DbApi
+import splitties.dimensions.dip
+import splitties.fragments.start
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -35,6 +42,26 @@ class CalendarFragment : Fragment() {
     private lateinit var binding: FragmentCalendarBinding
 
     private var listJob: Job? = null
+
+    private val manager: LinearLayoutManager
+        get() = binding.rvCalendar.layoutManager as LinearLayoutManager
+
+    private val scrollListener = object : EndlessListener() {
+
+        override fun onScrolledToTop() {
+            adapter.insertFirst()
+        }
+
+        override fun onScrolledToBottom() {
+            adapter.insertLast()
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                adapter.mState = manager.onSaveInstanceState()
+            }
+        }
+    }
 
     @SuppressLint("SetTextI18n")
     private val adapter = monthAdapter {
@@ -59,9 +86,26 @@ class CalendarFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val context = requireContext()
+        val state = adapter.mState
         binding.rvCalendar.adapter = adapter
+        if (state != null) {
+            manager.onRestoreInstanceState(state)
+        } else {
+            manager.scrollToPositionWithOffset(2, context.dip(24))
+        }
+        binding.rvCalendar.addOnScrollListener(scrollListener)
         binding.fabAdd.setOnClickListener {
+            start<BookingActivity>()
+        }
+        val filter = childFragmentManager.findFragmentById(R.id.f_filter) as FilterFragment
+        viewLifecycleOwner.lifecycleScope.launch {
+            filter.loadBuildings()
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            filter.buildings.collect {
 
+            }
         }
     }
 
